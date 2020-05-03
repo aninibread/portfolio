46f5aef0572947f723419195c69985788270ffba
aninibread <54481763+aninibread@users.noreply.github.com>
2020-05-02 22:49:00 -0400

    Fixed scroll

--- Scripts/reactNav.js
+++ Scripts/reactNav.js
@@ -1,39 +1,56 @@
-function navEffect() {
-  const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
+function navSlide() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-    burger.addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = "";
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.4
-          }s`;
-        }
-      });
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
-      burger.classList.toggle("toggle");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.4
+        }s`;
+      }
     });
-  };
+    burger.classList.toggle("toggle");
+  });
+}
+
+function navTrans() {
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > $("header").height() - 300) {
+      $("nav").addClass("black");
+    } else {
+      $("nav").removeClass("black");
+    }
+  });
+}
+
+if ($(window).width() > 768) {
+  navTrans();
   navSlide();
 }
 
-if ($(window).width() < 768) {
-  $("nav").addClass("shrink");
-  navEffect();
-} else {
-  $("nav").removeClass("shrink");
-}
-
-$(window).on("resize", function () {
-  if ($(window).width() < 768) {
+ssm.addState({
+  id: "mobile",
+  query: "(max-width: 767px)",
+  onEnter: function () {
     $("nav").addClass("shrink");
-    navEffect();
-  } else {
-    $("nav").removeClass("shrink");
-  }
+    navSlide();
+  },
 });
+
+ssm.addState({
+  id: "desktop",
+  query: "(min-width: 767px)",
+  onEnter: function () {
+    $("nav").removeClass("shrink");
+    navTrans();
+    navSlide();
+  },
+});
+
+ssm.ready();
--- Scripts/transNav.js
+++ Scripts/transNav.js
@@ -1,7 +1,9 @@
-$(window).on("scroll", function () {
-  if ($(this).scrollTop() > $("header").height() - 300) {
-    $("nav").addClass("black");
-  } else {
-    $("nav").removeClass("black");
-  }
-});
+function transNav() {
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > $("header").height() - 300) {
+      $("nav").addClass("black");
+    } else {
+      $("nav").removeClass("black");
+    }
+  });
+}
