2b58eb4e838c0da5535f5d397ce1ad76435aeab7
aninibread <54481763+aninibread@users.noreply.github.com>
2020-05-03 22:29:40 -0400

    add content-box

--- Scripts/transNav.js
+++
@@ -1,9 +0,0 @@
-function transNav() {
-  $(window).on("scroll", function () {
-    if ($(this).scrollTop() > $("header").height() - 300) {
-      $("nav").addClass("black");
-    } else {
-      $("nav").removeClass("black");
-    }
-  });
-}
